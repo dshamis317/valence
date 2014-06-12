16b1fb14a0546f9e27bb10d7a1b54253ea0ee171
dshamis317 <dshamis317@gmail.com>
2014-06-12 17:24:28 -0400

    db migrations and models created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+user = User.create(email: "bilbert@example.com", password: "password", username: "username")
+
+
+britney = Song.create(title: "Hit Me Baby One More Time", artist: "Britney, bitch")
+smith = Song.create(title: "I Don't Wanna Miss A Thing", artist: "Aerosmith")
+stones = Song.create(title: "Time Is On My Side", artist: "The Rolling Stones")
+
+party = Playlist.create(title: "Party Mix", mood: "happy", user_id: 2)
+
+party.songs << britney
+party.songs << smith
+party.songs << stones
+
+user.playlists << party
