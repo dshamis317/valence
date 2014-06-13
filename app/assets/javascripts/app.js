198f6685c56a517e819bf945e5738a9a081320d1
pantsonsteven <pantsonsteven@gmail.com>
2014-06-13 19:52:49 -0400

    adds proto modal to welcome#index

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,11 @@
+$(function() {
+   var $signUp = $('.signUp');
+
+   $signUp.click(function() {
+      var $div = $('<div>');
+
+      $div.addClass('signUpModal').css({'height': '500px', 'width': '500px', 'background-color': 'steelblue'}).html('Sign Up Here');
+
+      $('.container').append($div);
+   })
+})
