13f99c78cbee1c11914ddee8bcc356f7adfc65d9
dshamis317 <dshamis317@gmail.com>
2014-06-12 20:36:03 -0400

    added preview and image url to songs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140612203011) do
+ActiveRecord::Schema.define(version: 20140613003319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20140612203011) do
     t.float    "danceability"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_url"
+    t.string   "preview_url"
   end
 
   create_table "users", force: true do |t|
