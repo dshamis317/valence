d6e0ac5e66e921acb9a0c5da25e9f53f1c1a8d0b
dshamis317 <dshamis317@gmail.com>
2014-06-15 18:08:52 -0400

    working on creating json objects from search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -13,3 +13,10 @@ class WelcomeController < ApplicationController
     @song = MusicSearch.get_song(params[:id])
   end
 end
+
+
+# //     search_songs = MusicSearch.find_song(@title)
+#     binding.pry
+#     @songs = search_songs.each do |song|
+#       MusicSearch.get_song(song[:song_id])
+#     end
