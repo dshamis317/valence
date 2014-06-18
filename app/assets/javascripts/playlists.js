a50822eabce6c2a395fcc50ddc9c9e400d73d3b1
pantsonsteven <pantsonsteven@gmail.com>
2014-06-18 12:19:19 -0400

    changes variable names in visualization functions to be more comprehensible

--- app/assets/javascripts/playlists.js
+++ app/assets/javascripts/playlists.js
@@ -108,38 +108,20 @@ PlaylistCollection.prototype.fetch = function(callback) {
 function displaySongsOnShow () {
   var model = playlistCollection.playlists[0];
   var playlistView = new PlaylistView(model);
-  createPlaylistSongsAttrArr(model);
+  songsAttrArr(model);
   playlistView.render().el.appendTo($('.playlist-songs'));
 }
 
 
-// function updatePlaylistInfo(){
-//    var userId = $('.edit-form').data('userID');
-//    var playlistId = $('.edit-form').data('playlistId');
-//    var newTitle = $('.field').val();
-//    var newMood = $('.field').val();
-//    // need to update class names in jquery selector
-//    // change class of text field in form
-//    // create data object for post to db
-//    $.ajax({
-//       url: '/users/'+$userId+'/playlists/'+$playlistId
-//       method: 'put',
-//       dataType: 'json',
-//       success: function(data) {
-//          console.log('something')
-//       }
-//    })
-// }
 
+// ***** VISUALIZATION *****
 
 // constructs an object of all the song attributes of a given playlist's songs
-function createPlaylistSongsAttrArr(playlistCollectionObject){
+function songsAttrArr(playlistCollectionObject){
 
-   // gets array of song objects from playlistCollection
-   var playlistCollection = playlistCollectionObject;
-   var playlist = playlistCollection.songs;
+   var playlist = playlistCollectionObject.songs;
 
-   // creates empty arrays for all song attributes
+
         var titleArr = [],
            artistArr = [],
            energyArr = [],
@@ -155,7 +137,7 @@ function createPlaylistSongsAttrArr(playlistCollectionObject){
         image_urlArr = [],
       preview_urlArr = [];
 
-   // goes through song array and pushes each attribute into its corresponding attribute array
+
    $.each(playlist, function(i, song){
       titleArr.            push(song.title);
       artistArr.           push(song.artist);
@@ -173,7 +155,7 @@ function createPlaylistSongsAttrArr(playlistCollectionObject){
       preview_urlArr.      push(song.preview_url);
    });
 
-   //creates  songsAttributes object literal
+
    playlistSongsAttributes = {
             title : titleArr,
            artist : artistArr,
@@ -192,85 +174,88 @@ function createPlaylistSongsAttrArr(playlistCollectionObject){
    };
 
    return playlistSongsAttributes;
-}
 
-var d3Margin = {top: 10, right: 10, bottom: 10, left: 10};
+} // end of sonAttrArr
 
-var windowHeight = $(window).height();
 
-var d3Var = {
-      width: 650 - d3Margin.left - d3Margin.right,
-      height: (windowHeight * 0.9) - d3Margin.top - d3Margin.bottom,
-      padding: 5,
-};
+var svgMargin     = {top: 10, right: 10, bottom: 10, left: 10};
+
+var windowHeight  = $(window).height();
+
+var svgWindow     = {
+                  width: 650 - svgMargin.left - svgMargin.right,
+                  height: (windowHeight * 0.9) - svgMargin.top - svgMargin.bottom,
+                  padding: 5
+                  };
 
 
 function visualizePlaylist(playlistSongsAttributes) {
 
-   // these are the data arrays used in the visualization
-   var energy = playlistSongsAttributes.energy;
-   var danceability = playlistSongsAttributes.danceability;
+  // these are the data arrays used in the visualization
+  var energy = playlistSongsAttributes.energy;
+  var danceability = playlistSongsAttributes.danceability;
 
 
 
-   // height for song objects in relation to canvas height and number of objects in array
-   var songHeight = (d3Var.height/energy.length - d3Var.padding);
+  // height for song objects in relation to canvas height and number of objects in array
+  var songHeight = (svgWindow.height/energy.length - svgWindow.padding);
 
-// sets the y attribute in relation to the number of data elements entered and the height of the canvas
-   var y = d3.scale.ordinal()
-        .domain(d3.range(energy.length))
-        .rangePoints ([0, (d3Var.height-songHeight)]);
-
-
-   // creates an automatic color spectrum
-   var z = d3.scale.linear()
-        .domain([10,0])
-        .range(["hsl(300, 100%, 46%)","hsl(74, 100%, 93%)" ])
-        .interpolate(d3.interpolateHcl);
-
-   var canvas = d3.select('.canvas')
-           .attr("width", d3Var.width + d3Margin.left + d3Margin.right)
-           .attr("height", d3Var.height + d3Margin.top + d3Margin.bottom)
-           .append('g')
-           .attr("transform", "translate(" + d3Margin.left + "," + d3Margin.top + ")");
+  // sets the y attribute in relation to the number of data elements entered and the height of the canvas
+  var y = d3.scale.ordinal()
+      .domain(d3.range(energy.length))
+      .rangePoints ([0, (svgWindow.height-songHeight)]);
 
 
-   // creates songObjects as "g"s in "canvas" svg corresponding to the number of elements in data array, adds height and width and sets up a call back to slide function
-   canvas.selectAll('rect')
-      .data(energy)
-      .enter().append('rect')
-         .attr('width', d3Var.width*0.8)
-         .attr('height', songHeight)
-         .attr('x', d3Var.width/2 - 25)
-         .attr('y', function(d, i){ return y(i)})
-         .attr('rx', '2')
-         .attr('ry', '2')
-         .style('stroke-width', '2')
-         .style('stroke', 'darkgrey')
-      .transition()
-         .duration(function(d) { return 1100 - (1000*d) })
-         .each(function() {slide(danceability, z)});
+  // creates an automatic color spectrum
+  var z = d3.scale.linear()
+      .domain([10,0])
+      .range(["hsl(300, 100%, 46%)","hsl(74, 100%, 93%)" ])
+      .interpolate(d3.interpolateHcl);
 
-}
+  var canvas = d3.select('.canvas')
+      .attr("width", svgWindow.width + svgMargin.left + svgMargin.right)
+      .attr("height", svgWindow.height + svgMargin.top + svgMargin.bottom)
+      .append('g')
+      .attr("transform", "translate(" + svgMargin.left + "," + svgMargin.top + ")");
 
-// creates the lateral movement of the object according to dataArray
+
+     // creates songObjects as "g"s in "canvas" svg corresponding to the number of elements in data array, adds height and width and sets up a call back to slide function
+  canvas.selectAll('rect')
+    .data(energy)
+    .enter().append('rect')
+      .attr('width', svgWindow.width*0.8)
+      .attr('height', songHeight)
+      .attr('x', svgWindow.width/2 - 25)
+      .attr('y', function(d, i){ return y(i)})
+      .attr('rx', '2')
+      .attr('ry', '2')
+      .style('stroke-width', '2')
+      .style('stroke', 'darkgrey')
+    .transition()
+      .duration(function(d) { return 1100 - (1000*d) })
+      .each(function() {slide(danceability, z)});
+
+} // end of visualizePlaylist
+
+// creates the lateral movement of the object according to energy array
 function slide (danceability, z) {
-   var rectangle = d3.selectAll('rect');
-   (function repeat() {
-     rectangle = rectangle.transition()
-         .attr('x', d3Var.width - d3Var.width*0.8 )
-         .ease('linear')
-      .transition()
-         .attr('x', 0)
-         .ease('linear')
-         .each('end', repeat);
-   }) ();
+  var rectangle = d3.selectAll('rect');
+  (function repeat() {
+    rectangle = rectangle.transition()
+      .attr('x', svgWindow.width - svgWindow.width*0.8 )
+      .ease('linear')
+    .transition()
+      .attr('x', 0)
+      .ease('linear')
+      .each('end', repeat);
+  }) ();
 
-// sets color according to danceArray
+// sets color according to danceability array
 d3.select('.canvas').selectAll('rect')
-   .data(danceability)
-   .style('fill', function(d) { return z(d * 10 ); });
-}
+  .data(danceability)
+  .style('fill', function(d) { return z(d * 10 ); });
+
+} // end of slide
 
 
 
